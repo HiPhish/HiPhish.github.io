089046ba75ea0ccf860fa7c26c09968238dfb9bb
HiPhish <hiphish@Aleksandars-iMac.local>
2015-01-26 15:10:46 +0100

    Unified example pages.
    The scripts of the example pages have been moved into their own files
    and all example builds have the same name, allowing them to be
    identified by the same script.
    
    This makes the HTML of the example pages much smaller and removes
    redundancy.

---
+++ grid-framework/examples/web-player.js
@@ -0,0 +1,46 @@
+var config = {
+	width: 800, 
+	height: 600,
+	params: { enableDebugging:"0" }
+	
+};
+config.params["disableContextMenu"] = true;
+var u = new UnityObject2(config);
+
+jQuery(function() {
+
+	var $missingScreen = jQuery("#unityPlayer").find(".missing");
+	var $brokenScreen = jQuery("#unityPlayer").find(".broken");
+	$missingScreen.hide();
+	$brokenScreen.hide();
+
+	u.observeProgress(function (progress) {
+		switch(progress.pluginStatus) {
+			case "broken":
+				$brokenScreen.find("a").click(function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+					u.installPlugin();
+					return false;
+				});
+				$brokenScreen.show();
+			break;
+			case "missing":
+				$missingScreen.find("a").click(function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+					u.installPlugin();
+					return false;
+				});
+				$missingScreen.show();
+			break;
+			case "installed":
+				$missingScreen.remove();
+			break;
+			case "first":
+			break;
+		}
+	});
+	u.initPlugin(jQuery("#unityPlayer")[0], "example.unity3d");
+});
+
