408a8e46565207b1c348a8a081f2500bf7f39811
HiPhish <hiphish@Aleksandars-iMac.local>
2015-11-13 00:41:09 +0100

    new blogpost.

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,5 +1,14 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
 
+// Toggles the chevron of list items.
+$(function() {
+	$('.list-group-item').on('click', function() {
+	$('.glyphicon', this)
+		.toggleClass('glyphicon-chevron-right')
+		.toggleClass('glyphicon-chevron-down');
+	});
+});
+
 /* This file contains code related to the functioning of navigation bars. */
 $("document").ready( function() {
 	/* Display the caret on dropdown menues.         */
