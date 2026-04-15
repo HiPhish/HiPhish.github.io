05400463f80b1ac2b779230119831e23e73b4003
HiPhish <hiphish@posteo.de>
2026-04-15 17:19:19 +0200

    Implement a different global navigation bar

--- src/components/organism/GlobalNav.stories.jsx
+++ src/components/organism/GlobalNav.stories.jsx
@@ -1,37 +1,11 @@
 import GlobalNav from './GlobalNav.astro';
 
 export default {
-	title: 'Organism/Global navigation',
+	title: 'Organism/Global Navigation',
 	component: GlobalNav,
 	args: {
-		home: {
-			label: 'Dev Portfolio',
-			url: '#',
-		},
-		entries: [
-			{
-				label: 'Home',
-				url: '#',
-			}, {
-				label: 'Projects',
-				url: '#',
-				entries: [
-					{
-						label: 'Web apps',
-						url: '#',
-					}, {
-						label: 'APIs',
-						url: '#',
-					},
-				]
-			}, {
-				label: 'Blog',
-				url: '#',
-			}, {
-				label: 'Contact',
-				url: '#',
-			},
-		]
+		brand: 'Dev Portfolio',
+		home: '#',
 	},
 };
 
