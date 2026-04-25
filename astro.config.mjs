eeb4ce839a54a7826525aa85f55fdeb7f32d4a03
HiPhish <hiphish@posteo.de>
2026-04-26 01:05:17 +0200

    Add Alpine.js to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import summary from './src/plugins/remark/summary';
 import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hiphish.github.io',
 	compressHTML: false,
 	integrations: [
-		preact()
+		preact(),
+		alpinejs({entrypoint: '/src/alpine'}),
 	],
 	markdown: {
 		remarkPlugins: [summary],
