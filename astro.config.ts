78b88d81e67adb25345fc6b21a8a997147c91955
HiPhish <hiphish@posteo.de>
2026-07-27 21:31:57 +0200

    Remove unless comment

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,3 @@
-// 21:27:56 [WARN] [@astrojs/mdx] `remarkPlugins`, `rehypePlugins` on `mdx({...})` are deprecated. Pass them to `unified({...})` from `@astrojs/markdown-remark` and set it as `markdown.processor` instead — MDX will inherit them. Will be removed in a future major.
 import { defineConfig } from 'astro/config';
 import { unified } from '@astrojs/markdown-remark';
 
