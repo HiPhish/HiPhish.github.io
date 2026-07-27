753531e081d3377ac0294dd1baf40ce7c0135299
HiPhish <hiphish@posteo.de>
2026-07-27 21:22:48 +0200

    Pass Markdown plugins the modern way in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
 
 import preact from '@astrojs/preact';
 import summary from './src/plugins/remark/summary';
@@ -9,6 +10,15 @@ import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
 import mdx from '@astrojs/mdx';
 
+const remarkPlugins = [
+	summary,
+	remarkMath,
+];
+
+const rehypePlugins = [
+	rehypeMathML,
+];
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hiphish.github.io',
@@ -16,16 +26,16 @@ export default defineConfig({
 	integrations: [
 		preact(),
 		alpinejs({entrypoint: '/src/alpine'}),
-		mdx(),
+		mdx({
+			remarkPlugins,
+			rehypePlugins,
+		}),
 	],
 	markdown: {
-		remarkPlugins: [
-			summary,
-			remarkMath,
-		],
-		rehypePlugins: [
-			rehypeMathML,
-		]
+		processor: unified({
+			remarkPlugins,
+			rehypePlugins,
+		}),
 	},
 	vite: {
 		plugins: [tailwindcss()],
